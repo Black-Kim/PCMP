f2eb20793fd4426d6a7352a28ace226f81f1e44a
Black-Kim <kgh3680@gmail.com>
2020-12-02 08:59:10 +0900

    변경사항 변경

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -2,8 +2,15 @@ package controller;
 
 import java.util.Map;
 
+import java.util.Calendar;
 import service.BoardService;
+import service.FoodService;
+import service.PayService;
+import service.SalesService;
+import service.ScheduleService;
+import service.StaffService;
 import service.UserService;
+import service.WorkerBoardService;
 import service.WorkerService;
 import util.ScanUtil;
 import util.View;
@@ -12,15 +19,6 @@ import util.View;
 public class Controller {
 
 	public static void main(String[] args) {
-		/*
-		 * 발표순서 : 조 소개 > 주제 소개 > 주제 선정 배경 > 메뉴 구조 > 시연
-		 * 발표인원 : 발표자 1명, ppt 및 시연 도우미 1명
-		 * 
-		 * Controller : 화면 이동 -> 보기 ex)1,2,3 선택시 이동하게 구현
-		 * Service : 화면 기능 -> 이동시 서비스구현
-		 * Dao : 쿼리작성 ->데이터베이스 접속시 (select, insert update, delete)
-		 * 
-		 */
 		new Controller().start();
 	}
 	
@@ -31,9 +29,15 @@ public class Controller {
 	private UserService userService = UserService.getInstance();
 	private BoardService boardService = BoardService.getInstance();
 	private WorkerService workerService = WorkerService.getInstance();
-
+	private WorkerBoardService workerBoardService = WorkerBoardService.getInstance();
+	private PayService payService = PayService.getInstance();
+	private StaffService staffService = StaffService.getInstance();
+	private FoodService foodService = FoodService.getInstance();
+	private SalesService salesService = SalesService.getInstance();
+	private ScheduleService scheService = ScheduleService.getInstance();
 	private void start() {
 		int view = View.HOME;
+		
 		
 		while(true){
 			switch (view) {
@@ -58,54 +62,143 @@ public class Controller {
 			case View.SELECT_LIST:
 				view = boardService.selectList();
 				break;
-
-				//조회, 등록, 수정, 삭제를 구현해주세요
+			case View.WORKER_SELECT_LIST:
+				view = workerBoardService.selectList();
+				break;
+			case View.WORKER_BOARD_LIST:
+				view = workerBoardService.boardList();
+				break;
+			case View.WORKER_FlatRate_LIST:
+				view = workerBoardService.flatrateList();
+				break;
+			case View.PAY_FLATRATE:
+				view = payService.insertPayFlatRate();
+				break;
+			case View.PAY_FOOD:
+				view = foodService.insertPayFood();
+				break;
+			case View.WORKER_FIRST_PAGE:
+				view = workerBoardService.selectList();//알바 첫페이지
+				break;
+			case View.WORKER_MYPAGE:
+				view = workerBoardService.mypage(); //알바 마이페이지
+				break;
+			case View.ADMIN_FIRST_PAGE:
+				view = workerBoardService.admintList(); //총관리자 첫 페이지
+				break;
+			case View.ADMIN_MYPAGE:
+				view = workerBoardService.adminmypage(); //총관리자 마이페이지
+				break;
+			case View.SALES:
+				view = salesService.selectSales(); //매출 관리
+				break;
+			case View.STAFF_MN:
+				view = staffService.staffList(); //직원 관리
+				break;
+			case View.FOOD:
+				view = foodService.foodList(); //음식 관리
+				break;
+			case View.MY_PAGE:
+				view = userService.mypage();
+				break;
+			case View.SCHEDULE:
+				view = scheService.ScheduleList(); //스케줄 관리
+				break;
+			case View.WORK_SCHEDULE:
+				view = scheService.workScheduleList(); //스케줄 관리
+				break;
+			case View.USER_MN:
+			    view = workerBoardService.UserList();//회원 관리
+			    break;
 			}
 		}
 	}
 
 	private int home() {
-		System.out.println("----------------------------------");
-		System.out.println("1.PC방 회원\t2.관리자");
-		System.out.println("----------------------------------");
+		boolean power;
+		//System.out.println("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
+		System.out.println("■――――――――――――――――――――――――――――――――――――■");
+		System.out.println("■       수선호 PC방 에 오신걸 환영합니다           ■");
+		System.out.println("■                                    ■");
+		System.out.println("■  1.PC방 회원     2.근무자     0.프로그램 종료    ■");
+		System.out.println("■――――――――――――――――――――――――――――――――――――■");
+		System.out.println("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
+		
 		int input = ScanUtil.nextInt();
+		power = true;
+		while(power == true){
+			if(input == 1 || input == 2 || input == 0){
+				power = false;
+			}else{
+				System.out.println("잘못된 번호입니다.");
+				input = ScanUtil.nextInt();
+			}
+		}
 		switch(input){
 		case 1 : //회원
-			System.out.println("----------------------------------");
-			System.out.println("1.로그인\t2.회원가입\t0.프로그램 종료");
-			System.out.println("----------------------------------");
-			System.out.print("번호입력>");
-			input = ScanUtil.nextInt();
 			
+			System.out.println("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
+			System.out.println("■――――――――――――――――――――――――――――――――――――■");
+			System.out.println("■                                    ■");
+			System.out.println("■     1.로그인    2.회원가입   0.처음화면          ■");
+			System.out.println("■                                    ■");
+			System.out.println("■――――――――――――――――――――――――――――――――――――■");
+			System.out.println("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
+		
+			input = ScanUtil.nextInt();
+			power = true;
+			while(power == true){
+				if(input == 1 || input == 2 || input == 0){
+					power = false;
+				}else{
+					System.out.println("잘못된 번호입니다.");
+					input = ScanUtil.nextInt();
+					
+				}
+			}
 			switch(input){
 			case 1 : return View.LOGIN;
 			case 2 : return View.JOIN;
-			case 0 : 
-				System.out.println("프로그램이 종료되었습니다.");
-				System.exit(0);
+			case 0 : return View.HOME;
 			}
+				
+			
+			
+			
 		case 2 : //관리자
-			System.out.println("----------------------------------");
-			System.out.println("1.로그인\t2.관리자등록\t0.프로그램 종료");
-			System.out.println("----------------------------------");
-			System.out.print("번호입력>");
+			System.out.println("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
+			System.out.println("■――――――――――――――――――――――――――――――――――――■");
+			System.out.println("■                                    ■");
+			System.out.println("■    1.로그인    2.관리자등록    0.이전 화면       ■");
+			System.out.println("■                                    ■");
+			System.out.println("■――――――――――――――――――――――――――――――――――――■");
+			System.out.println("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
+			
 			input = ScanUtil.nextInt();
+			power = true;
+			while(power == true){
+				if(input == 1 || input == 2 || input == 0){
+					power = false;
+				}else{
+					System.out.println("잘못된 번호입니다.");
+					input = ScanUtil.nextInt();
+				}
+			}
 			
 			switch(input){
 			case 1 : return View.WORKERLOGIN;
 			case 2 : return View.WORKERJOIN;
-			case 0 : 
-				System.out.println("프로그램이 종료되었습니다.");
-				System.exit(0);
+			case 0 : return 0;
 			}
+		
+		case 0 : 
+			System.out.println("프로그램이 종료되었습니다.");
+			System.exit(0);
 		}
 		return View.HOME;
 	}
 
 }
-
-
-
 
 
 
